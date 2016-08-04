f3b280c0d646a9bcfd95c78172473c6c85cd9550
Asato Wakisaka <asato.wakisaka@gmail.com>
2016-08-04 18:01:55 +0900

    rather datetime (and kamipo traditional)

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,9 @@
+-- SET SESSION sql_mode='TRADITIONAL,NO_AUTO_VALUE_ON_ZERO,ONLY_FULL_GROUP_BY';
+
 CREATE TABLE user (
     `user_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `name` VARBINARY(32) NOT NULL,
-    `created` TIMESTAMP NOT NULL,
+    `created` DATETIME NOT NULL,
     PRIMARY KEY (user_id),
     UNIQUE KEY (name),
     KEY (created)
@@ -11,8 +13,8 @@ CREATE TABLE entry (
     `entry_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `url` VARBINARY(512) NOT NULL,
     `title` VARCHAR(512) NOT NULL,
-    `created` TIMESTAMP NOT NULL,
-    `updated` TIMESTAMP NOT NULL,
+    `created` DATETIME NOT NULL,
+    `updated` DATETIME NOT NULL,
     PRIMARY KEY (entry_id),
     UNIQUE KEY (url(191))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -22,8 +24,8 @@ CREATE TABLE bookmark (
     `user_id` BIGINT UNSIGNED NOT NULL,
     `entry_id` BIGINT UNSIGNED NOT NULL,
     `comment` VARCHAR(256) NOT NULL,
-    `created` TIMESTAMP NOT NULL,
-    `updated` TIMESTAMP NOT NULL,
+    `created` DATETIME NOT NULL,
+    `updated` DATETIME NOT NULL,
     PRIMARY KEY (bookmark_id),
     UNIQUE KEY (user_id, entry_id),
     KEY (user_id, created)
